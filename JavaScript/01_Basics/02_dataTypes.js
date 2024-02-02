fa1c2c8ef9dd2a24c4b876385c6564d6379c2c77
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-02-02 17:15:17 +0530

    Studied Datatypes

---
+++ JavaScript/01_Basics/02_dataTypes.js
@@ -0,0 +1,54 @@
+"use strict"; //This means treat all JS Code as newer version
+
+//  alert(3+3) //ye error de dega yaha pe browser me jake karo toh ho jayega //node me kar skte hai but alag syntak hoga uske liye 
+
+// semicolons are not mandatory in js but mind it your code should be readable 
+
+
+console.log(3+
+    3)//code readability should be high 
+
+console.log("Hemant")
+
+// Original documentation for JS
+
+// https://tc39.es/ecma262
+
+//But ye padhna bht tough ho jata hai toh you have 
+
+// MDN docs
+
+
+//Primitive Datatypes
+let name = "hitesh" //string
+let age = 18 //number
+let isloggedIn = true //boolean
+// let state = null
+let state;
+// number => 2 to power 53
+
+
+//big int 
+
+//string => ""
+
+//boolean => true/false
+
+//null //It is a standalone value in js
+//null matlab kuch nhi 
+
+//undefined => value abhi define nhi hui hai 
+
+//symbol => unique 
+
+
+
+//object 
+
+// ** null is an object 
+
+
+console.log(typeof "hitesh")//string
+console.log(typeof age) //number
+console.log(typeof null) //object 
+console.log(typeof undefined) //undefined
