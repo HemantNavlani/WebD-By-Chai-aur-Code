a88c2f755019ca639a3a71de19399021319c88c2
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-02-02 17:55:29 +0530

    comparisons of datatypes

---
+++ JavaScript/01_Basics/04_comparions.js
@@ -0,0 +1,27 @@
+// console.log(2>1)
+// console.log(2>=1);
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+console.log("2">1)
+console.log("02">1)
+
+
+//please avoid these type of conversions
+//try to write clean code
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined<0);
+console.log(undefined>=0);
+
+
+// === (strict check)
+
+console.log("2"==2)//true
+console.log("2"===2)//flase
