930b187ff17fc1650914816ff5c4ae8eb5760ff4
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-02-03 13:06:56 +0530

    datatypes summary

---
+++ JavaScript/01_Basics/datatypes_summary.js
@@ -0,0 +1,58 @@
+// Javascript is dynamically typed language
+//Null is an object
+
+
+// Data types are mainly of 2 types 
+// Primitive and Non-prmitive 
+// Kaise data ko memory me rakha jata hai aur kaise usko access kiya jata hai 
+
+//  Primitive
+// 7 types : String , Number, Boolean, null, undefined, Symbol, BigInt
+
+//Examples
+
+
+const score = 100;
+// const score = false;
+const scoreValue = 100.3;
+const isLoggedIn = false;
+
+const outsideTemp = null
+let userEmail;
+
+//symbol
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id)
+console.log(anotherId)
+console.log(id===anotherId)
+
+// bigInt
+
+const bigNumber = 21239939393993939939393n
+
+// Reference (Non- primitive)
+// Arrays, Objects, Functions
+
+//arrays
+const heros = ["shaktiman","naagraj","dpga"];
+
+let myObj = {
+    name:"hemant",
+    age:22
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof bigNumber)
+console.log(typeof outsideTemp)
+console.log(typeof myFunction) //function (isko kahte hai object function hai vaise)
+console.log(typeof heros)
+console.log(typeof id)
+
+// non - primitive data types ka typeof karne pe object hi aata hai 
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
