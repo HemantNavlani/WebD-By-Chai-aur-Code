363bb8a08d26da73b7961e6ebe06f0ca5da6679c
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-02-05 19:10:01 +0530

    object destructure and JSON Api

--- JavaScript/02_Basics/04_objects.js
+++ JavaScript/02_Basics/04_objects.js
@@ -63,3 +63,46 @@ console.log((Object.values(tinderUser)));
 console.log((Object.entries(tinderUser)));
 
 console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
+
+
+
+const course = {
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+// course.courseInstructor;//correect hai ye bhi 
+
+const {courseInstructor} = course//ye todha bdya tareeka hai 
+const {courseInstructor:instructor} = course//ye todha bdya tareeka hai 
+
+console.log(courseInstructor);
+console.log(instructor);
+
+
+// //For React 
+// const navbar = ({company})=>{
+
+// }
+// navbar(company = "hitesh")
+
+
+// Backend se data json me aata hai
+
+
+// Ye JSON data hai 
+// {
+//     "name" :"hitesh",
+//     "coursename" : "js in hindi",
+//     "price":"free"
+// }
+
+//Example// const api = "https://api.github.com/users/HemantNavlani" 
+
+//hamesha api me object hi mile aisa jaruri nhi hai 
