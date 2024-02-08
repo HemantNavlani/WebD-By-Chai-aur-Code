921335b8c9b44ccede85eac7098dea743f52e5b0
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-02-08 16:32:29 +0530

    scope in jhs

--- JavaScript/03_Basics/02_scopes.js
+++ JavaScript/03_Basics/02_scopes.js
@@ -16,9 +16,50 @@ if (true){
     let a = 10;
     const b = 20
     var c = 30
-    console.log("inner",a);
-}
+    // console.log("inner",a);
+} 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "hemant"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website)//error 
+    two()
+}
+// one()
+
+if (true){
+    const username = "hemant"
+    if (username==="hemant"){
+        const website = "youtube"
+        // console.log(username+website);
+    }
+    // console.log(website); //error
+}
+// console.log(username);//error
+
+// Interesting example ++++++++++++++++++++++++++++
+
+
+console.log( addone(5))
+
+function addone(num){
+    return num+1;
+}
+
+console.log( addone(5))
+
+
+console.log(addTwo(5))//error
+const addTwo = function(num){
+    return num+2
+}
+console.log(addTwo(5))
