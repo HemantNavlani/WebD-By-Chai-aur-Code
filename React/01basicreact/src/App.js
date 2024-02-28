39e7548050c896728365e730160aa201c0aa9a6b
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-02-28 15:52:04 +0530

    Making react projects - 2 ways

---
+++ React/01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Hare Krishna Hare Ram </h1>
+  );
+}
+
+export default App;
---
+++ React/01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hare Krishna Hare Ram | Vite React </h1>
+  )
+}
+
+export default App
